b99b164b5dfacf170e17d5ceff02289a07e7a4cb
Sulkaranay <sulkaranay03@gmail.com>
2025-02-05 03:09:29 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+
+<!doctype html>
+<html lang="en">
+
+	<?php include "include/head.php" ?>
+	<body>
+
+		<!-- Start Header/Navigation -->
+		<?php include "include/header.php" ?>
+		<!-- End Header/Navigation -->
+
+		<!-- Start Hero Section -->
+		<?php include "include/hero.php" ?>
+
+		<!-- End Hero Section -->
+
+		<!-- Start Product Section -->
+		<?php include "home.php" ?>
+
+		<!-- End Product Section -->
+
+		<!-- Start Why Choose Us Section -->
+
+		<?php include "include/choseus.php" ?>
+		<!-- End Why Choose Us Section -->
+
+		<!-- Start We Help Section -->
+		<?php include "include/help.php" ?>
+		<!-- End We Help Section -->
+
+		<!-- Start Popular Product -->
+		
+		<?php include "include/popular.php" ?>
+		<!-- End Popular Product -->
+
+		<!-- Start Testimonial Slider -->
+
+		<?php include "include/testimonimal.php" ?>
+
+		<!-- End Testimonial Slider -->
+
+		<!-- Start Blog Section -->
+		<?php include "include/recentblog.php" ?>
+		<!-- End Blog Section -->	
+
+		<!-- Start Footer Section -->
+		
+		<?php include "include/footer.php" ?>
+		<!-- End Footer Section -->	
+		 <?php include "include/script.php" ?>
+	</body>
+
+</html>
