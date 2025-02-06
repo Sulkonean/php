f57ef2ad7ee180fffcba5a41048df3492c1a4570
Sulkaranay <sulkaranay03@gmail.com>
2025-02-06 19:36:35 +0700

    Re-added admin as a regular folder

---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include "include/head.php" ?>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <?php include "include/sidebar.php" ?>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+        <?php include "include/topbar.php" ?>
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <?php include "home.php"?>
+        <!---Container Fluid-->
+      </div>
+      <!-- Footer -->
+        <?php include "include/footer.php" ?>
+      <!-- Footer -->
+    </div>
+  </div>
+
+  <!-- Scroll to top -->
+  <?php include "include/scriptandscrollontop.php" ?>
+</body>
+
+</html>
