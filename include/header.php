03ffeff7718e4dee1c1e4c7c8a24c20dad79596d
Sulkaranay <sulkaranay03@gmail.com>
2025-02-09 13:57:49 +0700

    Updated files with latest changes

--- include/header.php
+++ include/header.php
@@ -1,4 +1,4 @@
-<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
+<!-- <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
 <div class="container">
     <a class="navbar-brand" href="index.html">Furni<span>.</span></a>
@@ -13,14 +13,47 @@
                 <a class="nav-link" href="index.php">Home</a>
             </li>
             <li><a class="nav-link" href="index.php?p=shop">Shop</a></li>
-            <li><a class="nav-link" href="contact.php?p=contact">Contact us</a></li>
+            <li><a class="nav-link" href="index.php?p=contact">Contact us</a></li>
         </ul>
 
-        <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+        <ul class="custom-navbar-c ta navbar-nav mb-2 mb-md-0 ms-5">
             <li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
             <li><a class="nav-link" href="cart.html"><img src="images/cart.svg"></a></li>
         </ul>
     </div>
 </div>
     
+</nav> -->
+<nav class="custom-navbar navbar navbar-expand-md navbar-dark bg-dark" aria-label="Furni navigation bar">
+
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Furni<span>.</span></a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsFurni">
+            <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+                <?php 
+                    $p = isset($_GET['p']) ? $_GET['p'] : 'home'; 
+                ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($p == 'home') ? 'active' : '' ?>" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($p == 'shop') ? 'active' : '' ?>" href="index.php?p=shop">Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($p == 'contact') ? 'active' : '' ?>" href="index.php?p=contact">Contact Us</a>
+                </li>
+            </ul>
+
+            <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+                <li><a class="nav-link" href="#"><img src="images/user.svg" alt="User"></a></li>
+                <li><a class="nav-link" href="cart.html"><img src="images/cart.svg" alt="Cart"></a></li>
+            </ul>
+        </div>
+    </div>
+
 </nav>
--- index.php
+++ index.php
@@ -1,53 +1,85 @@
+<?php 
+    $page = "home.php";
+    $p = "home";
+	$hero = true;
+	$choseus = true;
+	$help = true;
+	$pupular = true;
+	$testnimonimal = true;
+	$recentblog = true;
+
+
+    if (isset($_GET['p'])) {
+        $p = $_GET['p']; // Assign the GET parameter to $p
+
+        switch ($p) {
+            case "shop":
+                $page = "shop.php";
+				$hero = false;
+                $choseus = false;
+				$help = false;
+				$pupular = false;
+				$testnimonimal = false;
+				$recentblog = false;
+                break;
+            case "contact":
+                $page = "contact.php";
+				$hero = false;
+				$choseus = false;
+				$help = false;
+				$pupular = false;
+				$testnimonimal = false;
+				$recentblog = false;
+                break;
+            default:
+                $page = "home.php"; // Fallback to home if an unknown value is passed
+        }
+    }
+?>
 
 <!doctype html>
 <html lang="en">
 
-	<?php include "include/head.php" ?>
-	<body>
+    <?php include "include/head.php"; ?>
+    <body>
 
-		<!-- Start Header/Navigation -->
-		<?php include "include/header.php" ?>
-		<!-- End Header/Navigation -->
+        <!-- Start Header/Navigation -->
+        <?php include "include/header.php"; ?>
+        <!-- End Header/Navigation -->
 
-		<!-- Start Hero Section -->
-		<?php include "include/hero.php" ?>
+        <!-- Start Hero Section -->
+        <?php if($hero) include "include/hero.php"; ?>
+        <!-- End Hero Section -->
 
-		<!-- End Hero Section -->
+        <!-- Start Product Section -->
+        <?php include "$page"; ?>
+        <!-- End Product Section -->
 
-		<!-- Start Product Section -->
-		<?php include "home.php" ?>
+        <!-- Start Why Choose Us Section -->
+        <?php if($choseus) include "include/choseus.php"; ?>
+        <!-- End Why Choose Us Section -->
 
-		<!-- End Product Section -->
+        <!-- Start We Help Section -->
+        <?php if($help) include "include/help.php"; ?>
+        <!-- End We Help Section -->
 
-		<!-- Start Why Choose Us Section -->
+        <!-- Start Popular Product -->
+        <?php if($pupular) include "include/popular.php"; ?>
+        <!-- End Popular Product -->
 
-		<?php include "include/choseus.php" ?>
-		<!-- End Why Choose Us Section -->
+        <!-- Start Testimonial Slider -->
+        <?php if($testnimonimal) include "include/testimonimal.php"; ?>
+        <!-- End Testimonial Slider -->
 
-		<!-- Start We Help Section -->
-		<?php include "include/help.php" ?>
-		<!-- End We Help Section -->
+        <!-- Start Blog Section -->
+        <?php if ($recentblog) include "include/recentblog.php"; ?>
+        <!-- End Blog Section -->    
 
-		<!-- Start Popular Product -->
-		
-		<?php include "include/popular.php" ?>
-		<!-- End Popular Product -->
+        <!-- Start Footer Section -->
+        <?php include "include/footer.php"; ?>
+        <!-- End Footer Section -->    
 
-		<!-- Start Testimonial Slider -->
-
-		<?php include "include/testimonimal.php" ?>
-
-		<!-- End Testimonial Slider -->
-
-		<!-- Start Blog Section -->
-		<?php include "include/recentblog.php" ?>
-		<!-- End Blog Section -->	
-
-		<!-- Start Footer Section -->
-		
-		<?php include "include/footer.php" ?>
-		<!-- End Footer Section -->	
-		 <?php include "include/script.php" ?>
-	</body>
+        <?php include "include/script.php"; ?>
+    </body>
 
 </html>
